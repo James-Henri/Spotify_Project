956e9f575802bf8dd35a693981739d76f1b55236
James Henriquez <jameshenriquez123@gmail.com>
2024-09-29 20:29:36 -0400

    restarted

--- start.py
+++ start.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, url_for, session, redirect
+from dotenv import load_dotenv
+import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -7,8 +9,12 @@ app = Flask(__name__)
 app.secret_key = "PleaseChange"
 app.config['SESSION_COOKIE_NAME'] = 'Users Cookie'
 
+def configure():    #configure will get the secret API Keys
+    load_dotenv()
+
 @app.route("/", methods = ['GET', 'POST'])
 def main_menu():
+    configure()         #must call configure in main section to make sure we can retrieve secret key
     if request.method == "POST":
         sp_oauth = create_spotify_oauth()
         auth_url = sp_oauth.get_authorize_url()
@@ -23,7 +29,7 @@ def response():
 def create_spotify_oauth():
     return SpotifyOAuth(
         client_id="dda07a58df584d4cad1aed5d3d056dae",
-        client_secret="6dff752e213d447fbbe8eb1a2422c6e5",
+        client_secret= os.getenv("api_secret"),
         redirect_uri=url_for('response', _external=True),
         scope="user-library-read"
     )
